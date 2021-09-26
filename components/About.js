5f69688a5de747c1f404a152d7bfe71e93a4ccd2
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-09-26 13:00:44 +0200

    navbar responsive on scroll

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div className="about-container flex bg-gray-100 h-screen p-4 md:p-16">
      <div className="hero grid grid-cols-8  bg-white justify-center items-center">
-      <h1 className="col-start-2 col-span-5 text-4xl leading-relaxed">
+      <h1 className="col-start-2 col-span-4 text-4xl leading-relaxed">
         Hello, I'm <span className="span line">Mick</span>,<br /> an experienced{" "}
         <span className="span2">graphic designer</span> and{" "}
         <span2 className="span2">dtp-operator.</span2> <br />Currently on a{" "}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,58 +1,66 @@
 import Link from "next/link";
 import Image from "next/dist/client/image";
 import { useRouter } from "next/dist/client/router";
+import { useState, Fragment } from "react";
 
 const Navbar = () => {
   const router = useRouter();
-  return (
-    <nav>
-      <div
-        className="navbar-wrapper fixed top-0 left-0 right-0 w-full z-10 bg-white" 
-      >
-        <div className={`navbar-container flex justify-between items-center mx-4 md:mx-16 py-4 ${
-          router.pathname !== "/" ? "bg-white border-b-2 border-green-600" : "bg-white border-b-2 border-green-600"
-        }`}>
-          <div className="logo-container">
-              <div className="logo">
-            <Link href="/">
-              <Image
-                src={
-                  router.pathname !== "/"
-                    ? "/MD_logo_courier.svg"
-                    : "/MD_logo_courier.svg"
-                }
-                alt="logo"
-                width={40}
-                height={40}
-              />
-              {/* <Image src="/MD_logo_courier.svg" alt="" width={30} height={30} /> */}
-            </Link>
-          </div>
-          </div>
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", changeNavbarColor);
+  }
 
-          <div className="navlinks">
-            <Link href="/" passHref>
-              <a
-                className={` ${
-                  router.pathname !== "/"
-                    ? "text-black hover:text-black"
-                    : "text-black hover:text-black"
-                }`}
-              >
-                {" "}
-                Home{" "}
-              </a>
-            </Link>
-            <Link href="/#projects" passHref>
-              <a className="text-black hover:text-white"> Projects </a>
-            </Link>
-            <Link href="/about" passHref>
-              <a className="text-black hover:text-white"> About </a>
-            </Link>
+  return (
+    <Fragment>
+      <nav>
+        <div className="navbar-wrapper fixed top-0 left-0 right-0 w-full z-10 bg-white">
+          <div
+            className={`navbar-container flex justify-between items-center mx-4 md:mx-16 py-4 ${
+              colorChange
+                ? "bg-white border-b-2 border-white"
+                : "bg-white border-b-2 border-green-600"
+            }`}
+          >
+            <div className="logo-container">
+              <div className="logo">
+                <Link href="/">
+                  <Image
+                    src={
+                      colorChange
+                        ? "/MD_logo_courier_green.svg"
+                        : "/MD_logo_courier.svg"
+                    }
+                    alt="logo"
+                    width={40}
+                    height={40}
+                  />
+                  {/* <Image src="/MD_logo_courier.svg" alt="" width={30} height={30} /> */}
+                </Link>
+              </div>
+            </div>
+
+            <div className="navlinks">
+              <Link href="/" passHref>
+                <a className={`${colorChange ? "text-green-600 hover:text-black" : "text-black hover:text-green-600"}`}>Home</a>
+              </Link>
+              <Link href="/#projects" passHref>
+              <a className={`${colorChange ? "text-green-600 hover:text-black" : "text-black hover:text-green-600"}`}>Projects</a>
+              </Link>
+              <Link href="/#about" passHref>
+              <a className={`${colorChange ? "text-green-600 hover:text-black" : "text-black hover:text-green-600"}`}>About</a>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </Fragment>
   );
 };
 

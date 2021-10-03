f288ec884b5a939cdb34bc7da2cf5cc455641d87
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-10-03 21:09:25 +0200

    about section

--- components/About.js
+++ components/About.js
@@ -15,17 +15,28 @@ const About = () => {
         <div className="grid grid-cols-1 md:grid-cols-8 gap-6 mx-6 md:mx-16 2xl:mx-40 md:pb-16 2xl:pb-32">
           <div className="about-text mt-6 md:mt-0 md:col-span-4 lg:col-span-6 2xl:col-start-1 ">
             <p className="text-xl leading-snug">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-              consequuntur, eligendi autem magnam eveniet molestiae officiis
-              explicabo atque tenetur rem debitis hic deserunt vitae quia nemo
-              laboriosam sit, iusto quod! Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Totam consequuntur, eligendi autem magnam
-              eveniet molestiae officiis explicabo atque tenetur rem debitis hic
-              deserunt vitae quia nemo laboriosam sit, iusto quod! Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Totam consequuntur,
-              eligendi autem magnam eveniet molestiae officiis explicabo atque
-              tenetur rem debitis hic deserunt vitae quia nemo laboriosam sit,
-              iusto quod!
+              Hello again, I'm Mick, a graphic designer, creative dtp-operator
+              and junior web developer from Antwerp. Nice to meet you.
+              <br></br>
+              <br></br>
+
+              Above are some of the projects I made during a six months full time training
+              at BeCode Antwerp, they represent the first steps I have been
+              taking in my ambition of becoming a web developer. More projects
+              can be found on my Github profile. 
+              <br></br>
+              <br></br>
+
+              Previously, during my career as a graphic designer I have worked on a broad range of projects:
+              making layouts for books, magazines, advertisments and news
+              papers; designing POS-materials, logo's, trade stands, interactive
+              magazines, websites and illustrations.
+              <br></br>
+              <br></br>
+              I would love to add a more dynamic and interactive layer to my skills and enter the world of
+              web development with it's endless posibilities and exciting
+              prospects. Currently I am actively looking for an internship and
+              continue my journey.
             </p>
           </div>
           <div className="portrait mb-6 md:mb-0 md:col-span-4 lg:col-span-2">

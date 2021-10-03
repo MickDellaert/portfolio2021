86beb30e1196518738459cc089c16f982967571e
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-10-03 16:46:54 +0200

    header position test

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,21 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import { ParallaxProvider } from "react-scroll-parallax";
+
 
 const Header = () => {
-    return ( <header className="header-container w-full flex flex-col justify-center">
-        <Navbar/>
-        <Hero/>
-    </header> );
-}
- 
+  return (
+      <>
+    <ParallaxProvider>
+
+    <header className="header-container relative w-full flex flex-col justify-center">
+      <Navbar />
+      <Hero />
+    </header>
+    </ParallaxProvider>
+    </>
+
+  );
+};
+
 export default Header;
